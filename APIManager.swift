60a7f66982c5676d9552f1e6f49d7aadcd2f4978
Bandi Anirudh <Anirudh@bandis-macbook-pro.local>
2016-04-26 02:30:28 +0530

    Completed Part 3

--- APIManager.swift
+++ APIManager.swift
@@ -24,21 +24,41 @@ class APIManager {
         
        let task = session.dataTaskWithURL(url!) { (data, response, error) -> Void in
         
+        if error != nil {
         
         dispatch_async(dispatch_get_main_queue()) {
-            if error != nil {
+            
                 completion(result: error!.localizedDescription)
             }
+        }
             else
             {
+                //Added for JSON Serialization
+                //print(data)
                 
-                completion(result: "NSURLSession Successful")
-                print(data)
-                
-            }
-        
-        
+                do {
+                    
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String : AnyObject] {
+                        
+                        print(json)
+                        
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        dispatch_async(dispatch_get_global_queue(priority , 0)){
+                            
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSerialization Successful")
+                            }
+                        }
+                    }
+                    
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: "Error in JSON Serialization")
+                    }
+                }
         }
+        
+        
         
         
         }
